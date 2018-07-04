4229ca8b0e8a169431c08ff86983ef036874335f
徐智伟 <xuzhiwei@i-focusing.com>
2018-07-04 17:26:42 +0800

    *

--- src/main/java/com/mrxu/cloud/vesta/VestaServiceApplication.java
+++ src/main/java/com/mrxu/cloud/vesta/VestaServiceApplication.java
@@ -1,5 +1,6 @@
 package com.mrxu.cloud.vesta;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.mrxu.cloud.*"})
+@EnableApolloConfig(value = {"vesta-service","commconfig"})
 public class VestaServiceApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(VestaServiceApplication.class).web(true).run(args);
